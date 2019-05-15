a1077c2c30eb73c6eb3442f30c1feb2d69128434
ShinoharaYuuyoru <ShinoharaYuuyoru@gmail.com>
2019-05-15 18:04:47 +0900

    unfinished

--- 1. Two Sum/1. Two Sum/1. Two Sum.cpp
+++ 1. Two Sum/1. Two Sum/1. Two Sum.cpp
@@ -1,8 +1,37 @@
 #include <iostream>
+#include <vector>
+#include <algorithm>
 
 using namespace std;
 
 int main()
 {
+	class Solution {
 
+	private:
+		typedef struct {
+			int num;
+			int indices;
+		}element;
+
+		bool comp(const element &a, const element &b) {
+
+		}
+
+	public:
+		vector<int> twoSum(vector<int>& nums, int target) {
+			vector<element> reNums;
+			int iterator = 0;
+			for (iterator = 1;iterator <= nums.size();iterator++)
+			{
+				element tempNums;
+				tempNums.num = nums[iterator];
+				tempNums.indices = iterator;
+
+				reNums.push_back(tempNums);
+			}
+
+
+		}
+	};
 }
