988cabf8308bede9bb8c4fb4eae22100cf3ae688
ShinoharaYuuyoru <ShinoharaYuuyoru@gmail.com>
2019-07-30 21:57:00 +0900

    604ms 5.04%, 8.7MB 78.99%. O(n3). Bad solution :-1: .

---
+++ 16. 3Sum Closest/16. 3Sum Closest.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+	int threeSumClosest(vector<int>& nums, int target) {
+		int closestSum = target;
+		int distance = INT_MAX;
+
+		sort(nums.begin(), nums.end());
+
+		vector<int>::iterator itr1;
+		for (itr1 = nums.begin(); itr1 != nums.end(); itr1++)
+		{
+			vector<int>::iterator itr2;
+			for (itr2 = itr1 + 1; itr2 != nums.end(); itr2++)
+			{
+				vector<int>::iterator itr3;
+				for (itr3 = itr2 + 1; itr3 != nums.end(); itr3++)
+				{
+					int threeSum = *itr1 + *itr2 + *itr3;
+					if (abs(target - threeSum) < distance)
+					{
+						distance = abs(target - threeSum);
+						closestSum = threeSum;
+					}
+				}
+			}
+		}
+
+		return closestSum;
+	}
+};
