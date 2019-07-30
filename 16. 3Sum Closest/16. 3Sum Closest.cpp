87108c2e0ae3ea0fc9b6482120700ecc6831dea8
ShinoharaYuuyoru <ShinoharaYuuyoru@gmail.com>
2019-07-30 22:03:14 +0900

    4ms 99.42%, 8.7MB 62.35%. O(n2) good solution :+1: .

--- 16. 3Sum Closest/16. 3Sum Closest.cpp
+++ 16. 3Sum Closest/16. 3Sum Closest.cpp
@@ -9,18 +9,30 @@ public:
 		vector<int>::iterator itr1;
 		for (itr1 = nums.begin(); itr1 != nums.end(); itr1++)
 		{
-			vector<int>::iterator itr2;
-			for (itr2 = itr1 + 1; itr2 != nums.end(); itr2++)
+			vector<int>::iterator itr2 = itr1 + 1;
+			vector<int>::iterator itr3 = nums.end() - 1;
+			while (itr2 < itr3)
 			{
-				vector<int>::iterator itr3;
-				for (itr3 = itr2 + 1; itr3 != nums.end(); itr3++)
+				int threeSum = *itr1 + *itr2 + *itr3;
+				if (abs(target - threeSum) < distance)
 				{
-					int threeSum = *itr1 + *itr2 + *itr3;
-					if (abs(target - threeSum) < distance)
-					{
-						distance = abs(target - threeSum);
-						closestSum = threeSum;
-					}
+					distance = abs(target - threeSum);
+					closestSum = threeSum;
+				}
+
+				if (threeSum < target)
+				{
+					itr2++;
+				}
+				else if (threeSum > target)
+				{
+					itr3--;
+				}
+				else
+				{
+					distance = abs(target - threeSum);
+					closestSum = threeSum;
+					return closestSum;
 				}
 			}
 		}
@@ -28,3 +40,37 @@ public:
 		return closestSum;
 	}
 };
+
+/*
+O(n3) bad solution.
+*/
+//class Solution {
+//public:
+//	int threeSumClosest(vector<int>& nums, int target) {
+//		int closestSum = target;
+//		int distance = INT_MAX;
+//
+//		sort(nums.begin(), nums.end());
+//
+//		vector<int>::iterator itr1;
+//		for (itr1 = nums.begin(); itr1 != nums.end(); itr1++)
+//		{
+//			vector<int>::iterator itr2;
+//			for (itr2 = itr1 + 1; itr2 != nums.end(); itr2++)
+//			{
+//				vector<int>::iterator itr3;
+//				for (itr3 = itr2 + 1; itr3 != nums.end(); itr3++)
+//				{
+//					int threeSum = *itr1 + *itr2 + *itr3;
+//					if (abs(target - threeSum) < distance)
+//					{
+//						distance = abs(target - threeSum);
+//						closestSum = threeSum;
+//					}
+//				}
+//			}
+//		}
+//
+//		return closestSum;
+//	}
+//};
