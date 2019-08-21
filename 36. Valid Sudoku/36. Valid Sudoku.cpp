defd22e84fffc27d18c293fe2bac74b63d79da13
ShinoharaYuuyoru <ShinoharaYuuyoru@gmail.com>
2019-08-22 00:50:22 +0900

    16ms 62.29%, 10.6MB 51.28%.

--- 36. Valid Sudoku/36. Valid Sudoku.cpp
+++ 36. Valid Sudoku/36. Valid Sudoku.cpp
@@ -1,35 +1,35 @@
 class Solution {
 public:
-    bool isValidSudoku(vector<vector<char>>& board) {
-        vector<vector<bool>> visitedCol(9, vector<bool>(9, false));
-        vector<vector<bool>> visitedRow(9, vector<bool>(9, false));
-        vector<vector<bool>> visitedBox(9, vector<bool>(9, false));
-        
-        for(int i=0; i<9; i++)
-        {
-            for(int j=0; j<9;j++)
-            {
-                if(board[i][j]=='.')
-                {
-                    continue;
-                }
-                
-                int num=board[i][j]-'1';
-                int boxIdx=i/3*3+j/3;
-                
-                if(visitedCol[i][num]==true||visitedRow[j][num]==true||visitedBox[boxIdx][num]==true)
-                {
-                    return false;
-                }
-                else
-                {
-                    visitedCol[i][num]=true;
-                    visitedRow[j][num]=true;
-                    visitedBox[boxIdx][num]=true;
-                }
-            }
-        }
-        
-        return true;
-    }
+	bool isValidSudoku(vector<vector<char>>& board) {
+		vector<vector<bool>> visitedCol(9, vector<bool>(9, false));
+		vector<vector<bool>> visitedRow(9, vector<bool>(9, false));
+		vector<vector<bool>> visitedBox(9, vector<bool>(9, false));
+
+		for (int i = 0; i < 9; i++)
+		{
+			for (int j = 0; j < 9; j++)
+			{
+				if (board[i][j] == '.')
+				{
+					continue;
+				}
+
+				int num = board[i][j] - '1';
+				int boxIdx = i / 3 * 3 + j / 3;
+
+				if (visitedCol[i][num] == true || visitedRow[j][num] == true || visitedBox[boxIdx][num] == true)
+				{
+					return false;
+				}
+				else
+				{
+					visitedCol[i][num] = true;
+					visitedRow[j][num] = true;
+					visitedBox[boxIdx][num] = true;
+				}
+			}
+		}
+
+		return true;
+	}
 };
