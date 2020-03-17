fe3080e671ef0cfdb637d5084baf30dca86a1bc2
ShinoharaYuuyoru <ShinoharaYuuyoru@gmail.com>
2020-03-17 18:34:12 +0900

    Two methods. See description.
    1. Hashtable
    20ms 16.72%, 11.3MB 23.68%.
    
    2. Two pointers: slow and fast ptrs.
    12ms 73.80%, 9MB 100.00%.

--- 141. Linked List Cycle/141. Linked List Cycle.cpp
+++ 141. Linked List Cycle/141. Linked List Cycle.cpp
@@ -52,35 +52,34 @@ public:
  */
 
  // 2. Two pointers: slow and fast ptrs.
- // 8ms 97.13%, 9.2MB 100.00%.
+ // 12ms 73.80%, 9MB 100.00%.
 class Solution {
 public:
     bool hasCycle(ListNode* head) {
-        if (head == NULL) {
+        if (head == NULL || head->next == NULL) {
             return false;
         }
 
         bool isCycle = false;
 
         ListNode* slowPtr = head;
-        ListNode* fastPtr = head->next;
+        ListNode* fastPtr = head;
         while (true) {
             if (slowPtr == NULL || fastPtr == NULL) {
                 isCycle = false;
                 break;
             }
             else {
-                if (slowPtr != fastPtr) {
-                    slowPtr = slowPtr->next;        // slowPtr step 1 forward.
-                    if (fastPtr->next != NULL) {
-                        fastPtr = fastPtr->next->next;      // fastPtr step 2 forward.
-                    }
-                    else {
-                        isCycle = false;
-                        break;
-                    }
+                slowPtr = slowPtr->next;
+                if (fastPtr->next == NULL) {
+                    isCycle = false;
+                    break;
                 }
                 else {
+                    fastPtr = fastPtr->next->next;
+                }
+
+                if (slowPtr == fastPtr) {
                     isCycle = true;
                     break;
                 }
